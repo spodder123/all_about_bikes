dee341558489b251d138926fbd50f78df1df0c5b
spodder123 <120617408+spodder123@users.noreply.github.com>
2023-07-13 03:24:05 -0500

    Data inserted in all tables of the database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'json'
 require 'open-uri'
-require "faker"
+require 'faker'
 
 # Fetch data from the API
 url = 'http://api.citybik.es/v2/networks'
@@ -11,15 +11,21 @@ data = JSON.parse(response.read)
 data['networks'].each do |network|
   country_name = network['location']['country']
   city_bike_name = network['name']
-  company = network['company'].first
+  company = network['company']&.first
 
   # Find or create the country
   country = Country.find_or_create_by(name: country_name)
 
   # Create the city bike under the country
-  country.city_bikes.create(
+  city_bike = country.city_bikes.create!(
     name: city_bike_name,
-    company: company,
+    company: company || 'Unknown', # Set a default value if company is missing
     city: network['location']['city']
   )
+
+  # Create random prices for the city bike in all countries
+  Country.all.each do |country|
+    price = Faker::Number.between(from: 200, to: 2000)
+    city_bike.prices.create!(price: price, country: country)
+  end
 end
