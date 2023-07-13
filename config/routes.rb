f852eae9c3e3e2f9af097f681d47a2f4e80ec99f
spodder123 <120617408+spodder123@users.noreply.github.com>
2023-07-13 00:30:51 -0500

    Generated and edited countries model and controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'countries/index'
+  get 'countries/show'
   get 'city_bikes/index'
   get 'city_bikes/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
