b65683cee99bf48b0cbda7ffe3449166ebade195
spodder123 <120617408+spodder123@users.noreply.github.com>
2023-07-13 01:35:53 -0500

    Generated staticpages controller to manage home and about page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'prices/index'
-  get 'prices/show'
-  get 'countries/index'
-  get 'countries/show'
-  get 'city_bikes/index'
-  get 'city_bikes/show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'static_pages#index'
+  get 'static_pages/about'
+  resources :city_bikes, only: [:index, :show]
+  resources :countries, only: [:index, :show]
+  resources :prices, only: [:index, :show]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
